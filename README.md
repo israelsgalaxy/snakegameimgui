8558d55b4f3d8007e3a8c898941a375b62a4bcdb
Israel Galadima <izzygaladima@gmail.com>
2024-04-07 00:11:35 +0100

    Use cmake and develop with linux

---
+++ README.md
@@ -0,0 +1,11 @@
+# Snakegame
+Classic snakegame built with DearImgui using GLFW and OpenGL backends
+
+## Dependencies
+On Debian-based Linux distros run `apt install libimgui-dev libglfw3-dev libstb-dev`
+
+## Building
+Run `cmake -S . -B build` in project root
+
+## Running
+Run `./build/snakegame` in project root
--- gui.h
+++ gui.h
@@ -4,21 +4,21 @@
 #include <glfw3.h>
 #include <imgui.h>
 
-namespace gui
-{
-	inline constexpr int WINDOW_HEIGHT = 450;
-	inline constexpr int WINDOW_WIDTH = 420;
-	inline GLFWwindow* window = nullptr;
+namespace gui {
+inline constexpr int WINDOW_HEIGHT = 450;
+inline constexpr int WINDOW_WIDTH = 420;
+inline GLFWwindow *window = nullptr;
 
-	void CreateGlfwWindow(std::string_view windowName);
-	void DestroyGlfwWindow();
+void CreateGlfwWindow(std::string_view windowName);
+void DestroyGlfwWindow();
 
-	void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+void KeyCallback(GLFWwindow *window, int key, int scancode, int action,
+                 int mods);
 
-	void CreateImgui();
-	void DestroyImgui();
+void CreateImgui();
+void DestroyImgui();
 
-	void PreRender();
-	void CreateContent();
-	void Render();
-}
+void PreRender();
+void CreateContent();
+void Render();
+} // namespace gui
