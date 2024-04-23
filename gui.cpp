7d03a40e11fd9f4f46d3321b44705b21dcad6983
Israel Galadima <izzygaladima@gmail.com>
2024-04-23 22:11:46 +0100

    Debloat main.cpp

--- gui.cpp
+++ gui.cpp
@@ -5,9 +5,12 @@
 #include <imgui_impl_opengl3.h>
 #include <imgui_impl_opengl3_loader.h>
 
-#include "gui.h"
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
+
+#include "gui.h"
+
+enum class gui::CellType { Food, Grid, SnakeHead, SnakeBody };
 
 void gui::CreateGlfwWindow(std::string_view windowName) {
   if (!glfwInit())
@@ -71,4 +74,151 @@ void gui::Render() {
   glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
   ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
   glfwSwapBuffers(window);
+}
+
+void gui::DrawCell(const std::pair<int, int> &cell, const CellType &cell_type) {
+  ImVec2 top_left(cell.first * CELL_SIZE, (cell.second * CELL_SIZE) + OFFSET_Y);
+  ImVec2 bottom_right((cell.first + 1) * CELL_SIZE,
+                      ((cell.second + 1) * CELL_SIZE) + OFFSET_Y);
+
+  ImDrawList *draw = ImGui::GetBackgroundDrawList();
+
+  ImU32 color_int;
+
+  switch (cell_type) {
+  case CellType::Food:
+    color_int = ImColor(ImVec4(0.18f, 0.67f, 0.17f, 1.0f));
+    break;
+  case CellType::Grid:
+    color_int = ImColor(ImVec4(0.2f, 0.2f, 0.2f, 1.0f));
+    break;
+  case CellType::SnakeHead:
+    color_int =
+        ImColor(ImVec4(252.f / 255.f, 224.f / 255.f, 176.f / 255.f, 1.f));
+    break;
+  case CellType::SnakeBody:
+    color_int =
+        ImColor(ImVec4(252.f / 255.f, 224.f / 255.f, 176.f / 255.f, 0.5f));
+    break;
+  }
+
+  if (cell_type == CellType::Grid) {
+    draw->AddRect(top_left, bottom_right, color_int);
+  } else {
+    draw->AddRectFilled(top_left, bottom_right, color_int);
+  }
+}
+
+void gui::DrawSnake(const std::pair<int, int> &snake_head,
+                    const std::deque<std::pair<int, int>> &snake_body) {
+  DrawCell(snake_head, CellType::SnakeHead);
+
+  for (const std::pair<int, int> &cell : snake_body) {
+    DrawCell(cell, CellType::SnakeBody);
+  }
+}
+
+void gui::CreateContent() {
+  static SnakeGame snake_game(GRID_SIZE);
+  static bool game_over = false;
+  static float elapsed_time = 0.0f; // time elapsed since last redraw of snake
+  static float delay_time =
+      0.5f; // minimum time that should elapse before redraw of snake
+  static float score = 0.0f;
+
+  ImGui::SetNextWindowPos({0, 0});
+  ImGui::SetNextWindowSize(
+      {static_cast<float>(WINDOW_WIDTH), static_cast<float>(WINDOW_HEIGHT)});
+  ImGui::Begin("Snake", nullptr,
+               ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse |
+                   ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoSavedSettings |
+                   ImGuiWindowFlags_NoTitleBar);
+
+  ImGui::Text("Score: %.1f", score);
+  ImGui::SameLine();
+  ImGui::Text(";");
+  ImGui::SameLine();
+  ImGui::Text("Speed: %.2f", -1 * delay_time);
+
+  for (int x = 0; x < GRID_SIZE; ++x) {
+    for (int y = 0; y < GRID_SIZE; ++y) {
+      DrawCell({x, y}, CellType::Grid);
+    }
+  }
+
+  DrawSnake(snake_game.GetHeadPosition(), snake_game.GetBodyPositions());
+  DrawCell(snake_game.GetFoodPosition(), CellType::Food);
+
+  if (game_over) {
+    ImGui::OpenPopup("Game Over");
+
+    const ImVec2 &center = ImGui::GetMainViewport()->GetCenter();
+    ImGui::SetNextWindowPos(center, ImGuiCond_Appearing, ImVec2(0.5f, 0.5f));
+
+    if (ImGui::BeginPopupModal("Game Over", NULL,
+                               ImGuiWindowFlags_AlwaysAutoResize |
+                                   ImGuiWindowFlags_NoMove)) {
+      if (ImGui::Button("Restart")) {
+        delay_time = 0.5f;
+        score = 0.0f;
+        user_direction = SnakeGame::Direction::Halt;
+        snake_game.Reset();
+        game_over = false;
+        ImGui::CloseCurrentPopup();
+      }
+      ImGui::EndPopup();
+    }
+
+    ImGui::End();
+    return;
+  }
+
+  ImGuiIO &io = ImGui::GetIO();
+  elapsed_time += io.DeltaTime;
+
+  if (elapsed_time >= delay_time) {
+    if (!(user_direction == SnakeGame::Direction::Halt)) {
+      switch (snake_game.MoveSnake(user_direction)) {
+      case SnakeGame::MoveResult::Body:
+        game_over = true;
+        break;
+      case SnakeGame::MoveResult::Grid:
+        break;
+      case SnakeGame::MoveResult::Food:
+        score += 0.2f;
+        delay_time =
+            (delay_time > MIN_DELAY_TIME) ? delay_time - 0.02f : MIN_DELAY_TIME;
+        break;
+      };
+    }
+
+    elapsed_time = 0;
+  }
+
+  ImGui::End();
+}
+
+void gui::KeyCallback(GLFWwindow *window, int key, int scancode, int action,
+                      int mods) {
+  ImGuiIO &io = ImGui::GetIO();
+  // Handle keyboard input if not already captured
+  if (!io.WantCaptureKeyboard && action == GLFW_PRESS) {
+    switch (key) {
+    case GLFW_KEY_LEFT:
+      user_direction = SnakeGame::Direction::Left;
+      break;
+    case GLFW_KEY_RIGHT:
+      user_direction = SnakeGame::Direction::Right;
+      break;
+    case GLFW_KEY_DOWN:
+      user_direction = SnakeGame::Direction::Down;
+      break;
+    case GLFW_KEY_UP:
+      user_direction = SnakeGame::Direction::Up;
+      break;
+    case GLFW_KEY_SPACE:
+      user_direction = SnakeGame::Direction::Halt;
+      break;
+    }
+  }
 }
--- gui.h
+++ gui.h
@@ -1,13 +1,24 @@
-#pragma once
+#ifndef GUI_H
+#define GUI_H
 #include <string_view>
 
 #include <glfw3.h>
 #include <imgui.h>
 
+#include "snakegame.h"
+
 namespace gui {
 inline constexpr int WINDOW_HEIGHT = 450;
 inline constexpr int WINDOW_WIDTH = 420;
+inline constexpr float GRID_SIZE = 20;
+inline constexpr float CELL_SIZE = WINDOW_WIDTH / GRID_SIZE;
+inline constexpr float OFFSET_Y = WINDOW_HEIGHT - WINDOW_WIDTH;
+inline constexpr float MIN_DELAY_TIME = 0.1f;
+
 inline GLFWwindow *window = nullptr;
+inline SnakeGame::Direction user_direction = SnakeGame::Direction::Halt;
+
+enum class CellType;
 
 void CreateGlfwWindow(std::string_view windowName);
 void DestroyGlfwWindow();
@@ -21,4 +32,9 @@ void DestroyImgui();
 void PreRender();
 void CreateContent();
 void Render();
+
+void DrawCell(const std::pair<int, int> &cell, const CellType &cell_type);
+void DrawSnake(const std::pair<int, int> &snake_head,
+               const std::deque<std::pair<int, int>> &snake_body);
 } // namespace gui
+#endif
